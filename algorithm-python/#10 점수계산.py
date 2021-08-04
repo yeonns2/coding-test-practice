ef186d46f9a2a15a1aec9d9edf24bb19a20b8e5d
Jungyeon <yeonns2@gmail.com>
2021-08-04 12:30:40 +0900

    점수계산 코드 개선

--- algorithm-python/#10 점수계산.py
+++ algorithm-python/#10 점수계산.py
@@ -1,14 +1,14 @@
 n=int(input())
 score = list(map(int, input().split()))
-cnt=[0]*n
-flag = 0
-for i in range(n):
-    if score[i]== 0 :
-        flag = 0
-    elif flag == 0 :
-        cnt[i]=1
-        flag = 1
-    else :
-        cnt[i]=cnt[i-1]+1  
 
-print(sum(cnt))
+cnt=0
+sum=0
+
+for x in score:
+    if x==0:
+        cnt=0
+    else :
+        cnt+=1
+        sum+=cnt
+
+print(sum)
