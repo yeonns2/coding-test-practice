82f6397d8a8fbe34d926553f8645a0ae8fb14c59
Jungyeon <yeonns2@gmail.com>
2021-08-07 13:31:39 +0900

    2021.08.07 숫자만 추출

--- algorithm-python/#11 회문 문자열 검사.py
+++ algorithm-python/#11 회문 문자열 검사.py
@@ -5,8 +5,7 @@ for i in range(n):
     s = s.lower()
 
     if(list(s) == list(reversed(s))):
-        print('# YES')
+        print('#%d YES' %(i+1))
     else : 
-        print('# NO')
+        print('#%d NO' %(i+1))
    
-    
