9a4b79de613ddfebc0104e9208dffb97d60e2b1a
yeonns2 <yeonns2@gmail.com>
2022-01-15 23:31:50 +0900

    2022.01.15 백준 11047

--- 백준/11047.cpp
+++ 백준/11047.cpp
@@ -1,13 +1,29 @@
-n, k = map(int, input().split())
-m = []
-num = 0
-for i in range(n):
-    m.append(int(input()))
-for i in range(n - 1, -1, -1):
-    if k == 0:
-        break
-    if m[i] > k:
-        continue
-    num += k // m[i]
-    k %= m[i]
-print(num)
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int cmp(int a, int b) {
+	return a > b;
+}
+
+int main() {
+	int n, k, ans = 0;
+	cin >> n >> k;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+  
+	sort(v.begin(), v.end(),cmp);
+
+	for (int i = 0; i < n; i++) {
+		while (k - v[i] >= 0) {
+			ans++;
+			k -= v[i];
+		}
+	}
+	cout << ans << '\n';
+}
