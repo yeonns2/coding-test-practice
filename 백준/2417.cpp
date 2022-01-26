7faa0cc137c24305dab6635aa0bd62499648fd11
Jungyeon <46320067+yeonns2@users.noreply.github.com>
2022-01-26 23:20:06 +0900

    [2417] 정수 제곱근

---
+++ 백준/2417.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+    
+    long long result = ceil(sqrt(n));
+    
+    cout << result<< endl;
+
+    retu
